6ad27cd43b1a1d2b179d8de0b2c9aa477b3bd895
Lakshay Bhardwaj <lakshy.bhardwaj002@gmail.com>
2020-02-21 14:41:52 +0530

    Update CreateAlbumDialog.xaml.cs

--- WpfAlbus/CreateAlbumDialog.xaml.cs
+++ WpfAlbus/CreateAlbumDialog.xaml.cs
@@ -40,8 +40,8 @@ namespace WpfAlbus
         {
             string connstring = String.Format("Server={0};Port={1};" +
                             "User Id={2};Password={3};Database={4};",
-                            "localhost", 5432, "postgres",
-                            "thinksys@123", "albuswpf");
+                            "localhost", 5432, "",
+                            "", "");
             // Making connection with Npgsql provider
             NpgsqlConnection conn = new NpgsqlConnection(connstring);
             conn.Open();
